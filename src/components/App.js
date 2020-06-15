6710cad16526d4b3dd7b4f8bdc44d81d961cc07a
pseudowalrus <pseudowalrus@protonmail.com>
2020-06-15 17:55:42 +0100

    Set up Express API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Home from "../pages/Home";
 import HallOfFame from "../pages/HallOfFame";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+
+
 const App = () => {
+  const [apiResponse, setApiResponse] = useState(); 
+
+  const callAPI = () => {
+    fetch("https://localhost:9000/demoAPI")
+      .then(res => res.text())
+      .then(res => setApiResponse(res))
+      .catch(err => err);
+  }
+
+  useEffect(() => {
+    callAPI(); 
+  })
+
   return (
     <div id="app">
+      <p className="response-text">Response: {apiResponse}</p>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
